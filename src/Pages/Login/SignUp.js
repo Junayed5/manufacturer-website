a63661dddc3508c4e6f7c933e998f20418b02623
Junayed5 <mdjunayed352003@gmail.com>
2022-05-23 12:05:03 +0600

    Create Signup form

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -2,19 +2,32 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const SignUp = () => {
 
     const { register, formState: { errors }, handleSubmit } = useForm();
 
     const onSubmit = data => {
         console.log(data)
     };
+
     return (
         <div className='flex justify-center'>
             <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <h2 className='text-3xl font-bold text-primary text-center'>Please Login</h2>
+                <h2 className='text-3xl font-bold text-primary text-center'>Please Register</h2>
                 <div className="card-body">
                     <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="name"
+                                className="input input-bordered"
+                                {...register("name", { required: true })}
+                            />
+                            {errors.name?.type === 'required' && <span className='text-red-600'>Name is required</span>}
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -45,14 +58,11 @@ const Login = () => {
                             />
                             {errors.password?.type === 'required' && <span className='text-red-600'>{errors?.password?.message}</span>}
                             <label className="label">
-                                <Link to="/signUp" className="label-text-alt link link-hover">New to Computer parts?</Link>
-                            </label>
-                            <label className="label">
-                                <a href="/" className="label-text-alt link link-hover">Forgot password?</a>
+                                <Link to="/login" className="label-text-alt link link-hover">Already have an account?</Link>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary text-white">Login</button>
+                            <button className="btn btn-primary text-white">Sign Up</button>
                         </div>
                         <div className="divider">OR</div>
                         
@@ -66,4 +76,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
