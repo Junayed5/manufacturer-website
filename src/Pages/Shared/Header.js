4c78f68171982eec59a0a4a082a9baae734e7b57
Junayed5 <mdjunayed352003@gmail.com>
2022-05-23 12:59:53 +0600

    sign out

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,11 +1,20 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Header = () => {
 
+    const [user, loading, error] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
+
     const navigation = <>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/login'>Login</Link></li>
+        {user ? <button onClick={handleSignOut} className='btn btn-ghost'>Sign out</button>:<li><Link to='/login'>Login</Link></li>}
     </>
     return (
         <div className="navbar bg-base-100">
