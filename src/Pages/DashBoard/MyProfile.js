4629ebd7a915e090ef74c7b3b1cc512a3deb3f0d
Junayed5 <mdjunayed352003@gmail.com>
2022-05-24 23:28:29 +0600

    set my profile

--- src/Pages/DashBoard/MyProfile.js
+++ src/Pages/DashBoard/MyProfile.js
@@ -4,10 +4,21 @@ import auth from '../../firebase.init';
 
 const MyProfile = () => {
     const [user] = useAuthState(auth);
+    console.log(user);
     return (
-        <div>
-            <h1>Profile</h1>
-            <h3>{user.displayName}</h3>
+        <div className="card w-96 bg-base-100 shadow-xl mx-auto">
+            <div className="card-body">
+                <div class="avatar">
+                    <div class="w-24 mask mask-squircle mx-auto">
+                        <img src={user.photoURL ? user.photoURL : 'https://api.lorem.space/image/face?hash=3174'} alt=''/>
+                    </div>
+                </div>
+                <p>Name:{user.displayName}</p>
+                <p>Email:{user.email}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
