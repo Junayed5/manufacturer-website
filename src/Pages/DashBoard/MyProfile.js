d6b7b685413ae9bf2e5895409d6a460dba4071d1
Junayed5 <mdjunayed352003@gmail.com>
2022-05-25 10:34:04 +0600

    sent database to profile update

--- src/Pages/DashBoard/MyProfile.js
+++ src/Pages/DashBoard/MyProfile.js
@@ -9,24 +9,35 @@ const MyProfile = () => {
     const { register, handleSubmit } = useForm();
     const onSubmit = data => {
         console.log(data);
+        fetch(`http://localhost:4000/profile/${user.email}`,{
+            method:'PUT',
+            headers: {
+                'Content-type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log({'success': data});
+        })
     }
     return (
         <div className="card w-96 bg-base-100 shadow-xl mx-auto">
             <div className="card-body">
-                <div class="avatar">
-                    <div class="w-24 mask mask-squircle mx-auto">
+                <div className="avatar">
+                    <div className="w-24 mask mask-squircle mx-auto">
                         <img src={user.photoURL ? user.photoURL : 'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0='} alt='' />
                     </div>
                 </div>
                 <p>Name:{user.displayName}</p>
                 <p>Email:{user.email}</p>
 
-                <label for="my-modal-3" class="btn modal-button btn-accent">Update Profile</label>
+                <label htmlFor="my-modal-3" className="btn modal-button btn-accent">Update Profile</label>
 
-                <input type="checkbox" id="my-modal-3" class="modal-toggle" />
-                <div class="modal">
-                    <div class="modal-box relative">
-                        <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                <div className="modal">
+                    <div className="modal-box relative">
+                        <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="form-control">
                                 <label className="label">
